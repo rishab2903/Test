0daeaa77f952faba72518f67714e8ab1a65a1c16
rishab2903 <rishaba@mail.usf.edu>
2019-09-10 21:40:33 -0400

    ty

---
+++ Test1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hellos world");
+            Console.WriteLine("hellose world");
+        }
+    }
+}
